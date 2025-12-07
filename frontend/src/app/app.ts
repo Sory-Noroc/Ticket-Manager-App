927cdd40fd68760973352e2f4331badb2408cd41
Sory02 <sorin.noroc@student.tuiasi.ro>
2025-12-07 23:59:52 +0200

    Login page design done

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+
+// https://material.angular.dev/components/categories
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('frontend');
+}
+
